21e8c4b7d3c6ec5cb3d3423d230a0dab92d5a622
ymjlabMax <sud665@ymjlab.com>
2024-03-11 16:41:18 +0900

    feat 효과 추가

---
+++ scroll-4/js/script.js
@@ -0,0 +1,17 @@
+let scrollNum = 0;
+
+const imageAll = document.querySelectorAll(".imageWrap .parallaxImage");
+const totalNum = imageAll.length;
+const subPageImage = document.querySelector(".subPage .parallaxImage");
+
+window.addEventListener("scroll", () => {
+    scrollNum = window.scrollY;
+
+    if (scrollNum < 2500) {
+        imageAll.forEach((item, index) => {
+            item.style.transform = `perspective(400px) translate3d(0,0,${
+                scrollNum / (2 * (totalNum - index))
+            }px)`;
+        });
+    }
+});
